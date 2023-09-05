95ec6dc90bc0bb781defff686c3bc2d239527713
codeoz <codeoz@proton.me>
2023-09-05 16:55:46 +0300

    clerk

--- app/sign-in/[[...sign-up]]/page.tsx
+++ app/sign-in/[[...sign-up]]/page.tsx
@@ -1,9 +1,14 @@
+'use client'
+
 import { SignIn } from '@clerk/nextjs'
+import { dark } from '@clerk/themes'
+import { useTheme } from 'next-themes'
 
 export default function SignInPage() {
+  const { theme } = useTheme()
   return (
     <div className='flex items-center justify-center h-[calc(100vh-110px)] overflow-y-hidden'>
-      <SignIn />
+      <SignIn appearance={{ baseTheme: theme === 'dark' ? dark : undefined }} />
     </div>
   )
 }
--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,9 +1,14 @@
+'use client'
+
 import { SignUp } from '@clerk/nextjs'
+import { dark } from '@clerk/themes'
+import { useTheme } from 'next-themes'
 
 export default function SignUpPage() {
+  const { theme } = useTheme()
   return (
     <div className='flex items-center justify-center h-[calc(100vh-110px)] overflow-y-hidden'>
-      <SignUp />
+      <SignUp appearance={{ baseTheme: theme === 'dark' ? dark : undefined }} />
     </div>
   )
 }
